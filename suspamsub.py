299b5e0abdd3604d839c9103b3bba99b890a0ada
Segloser <retraining@segloser.com>
2016-02-21 12:07:34 +0100

    Update suspamsub.py

--- suspamsub.py
+++ suspamsub.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ###########################################  LICENSE  ################################################
-All rights reserved.
+"""All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted
  provided that the following conditions are met:
